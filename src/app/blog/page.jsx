54b6d96618e8c019824287d24631af30eef0eef2
leokatsuki <leok-12@hotmail.com>
2023-12-30 10:17:50 -0300

    url

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -2,7 +2,7 @@ import Card from '@/components/card/Card'
 import styles from './blogPage.module.css'
 
 const getData = async () => {
-    const res = await fetch("https://verdant-pika-6abce7.netlify.app/api/posts", {
+    const res = await fetch("/api/posts", {
         cache: "no-store"
     })
 
